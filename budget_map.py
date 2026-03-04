3cff0a71f6a6b28e0e58cd6831cc25f90957a585
Noah Steinberg <nastein@umich.edu>
2026-03-04 15:32:12 -0500

    Added budget map that lets you track your spending vs your budget

---
+++ budget_map.py
@@ -0,0 +1,15 @@
+BUDGET_MAP = {
+    "Food": ["Groceries", "Dining"],   # combined
+    "Transportation": ["Transportation"],
+    "Clothes": ["Clothes"],
+    "Gifts": ["Gifts"],
+    "Misc": ["Health", "Entertainment"]
+}
+
+BUDGET_KEYS = {
+    "Food": "food_in",
+    "Transportation": "trans_travel_in",
+    "Clothes": "clothes_in",
+    "Gifts": "gifts_in",
+    "Misc": None
+}
